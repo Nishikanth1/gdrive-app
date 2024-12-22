fb2f15c03d37135bca8cd0000ec4ef1beb584e2d
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2024-12-22 16:09:09 +0530

    fix os environ

--- config/constants.py
+++ config/constants.py
@@ -5,7 +5,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CLIENT_SECRETS_PATH = os.get("CLIENT_SECRETS_PATH","/tmp/gdrive-app/cred/")
-CLIENT_TOKEN_PATH = os.get("CLIENT_TOKEN_PATH","/tmp/gdrive-app/token/")
-TEMP_FILE_STORAGE_PATH = os.get("TEMP_FILE_STORAGE_PATH","/tmp/gdrive-app/files/")
-TEMP_FILE_DOWNLOAD_PATH = os.get("CLIENT_SECRETS_PATH","/tmp/gdrive-app/downloads/")
+CLIENT_SECRETS_PATH = os.environ.get("CLIENT_SECRETS_PATH","/tmp/gdrive-app/cred/")
+CLIENT_TOKEN_PATH = os.environ.get("CLIENT_TOKEN_PATH","/tmp/gdrive-app/token/")
+TEMP_FILE_STORAGE_PATH = os.environ.get("TEMP_FILE_STORAGE_PATH","/tmp/gdrive-app/files/")
+TEMP_FILE_DOWNLOAD_PATH = os.environ.get("CLIENT_SECRETS_PATH","/tmp/gdrive-app/downloads/")
